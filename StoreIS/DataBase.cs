817f92f1b7350139a53aa72ffd7115306db087d9
18ipp27 <tk4chuk.16@gmail.com>
2022-05-11 11:51:33 +0300

    Add class Database & modify debug files

---
+++ StoreIS/DataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreIS
+{
+    class DataBase
+    {
+        private static Entities _context;
+        public static Entities GetEntities()
+        {
+            if (_context == null)
+                _context = new Entities();
+
+            return _context;
+        }
+    }
+}
