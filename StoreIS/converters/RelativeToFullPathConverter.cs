29f4a99a53d36a36fd33577d1e6bcf055eaaed6d
tkachukypec <tk4chuk.16@gmail.com>
2022-05-12 12:42:35 +0300

    Add new class RelativeToFullPathConverter

---
+++ StoreIS/converters/RelativeToFullPathConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.IO;
+
+namespace StoreIS.converters
+{
+    class RelativeToFullPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string path = value as string;
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                return "../resources/images/noImage.png";
+            }
+            try
+            {
+                return !File.Exists(path) ? "../resources/images/noImage.png" : Path.GetFullPath(path);
+            }
+            catch(Exception)
+            {
+                return "../resources/images/noImage.png";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
