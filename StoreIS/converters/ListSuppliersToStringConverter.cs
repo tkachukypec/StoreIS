3a108d0f528c2272822df04d03c4c835a7cc984c
tkachukypec <tk4chuk.16@gmail.com>
2022-05-13 12:44:50 +0300

    Add converter ListSuppliersToStringConverter

---
+++ StoreIS/converters/ListSuppliersToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.IO;
+
+namespace StoreIS.converters
+{
+    class ListSuppliersToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            ICollection<Supplier> suppliers = value as ICollection<Supplier>;
+            return string.Join(", ", suppliers.Select(p => p.Name));
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
